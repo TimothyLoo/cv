d5c9bd0ebb7beaa26f78df89946ba34d4a15e1af
Timothy Loo <TimothyLoo07@gmail.com>
2022-07-06 07:19:28 -1000

    finished game with simonGame.js

--- Simon Game Challenge Starting Files/simonGame.js
+++ Simon Game Challenge Starting Files/simonGame.js
@@ -3,7 +3,7 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userCLickedPattern = [];
 var level = 0;
-var started = new Boolean(false);
+var started = false;
 
 
 // plays sound
@@ -14,25 +14,33 @@ function playSound(name) {
 
 // animate press
 function animatePress(currentColour) {
-  setTimeout($("#btn").addClass("pressed"),100);
+  $('#' + currentColour).addClass('pressed');
+  setTimeout(function () {
+    $('#' + currentColour).removeClass('pressed');
+  }, 100);
 }
 
 // detect if button is click, append user click to user gamePattern
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userCLickedPattern.push(userChosenColour);
+
   playSound(userChosenColour);
+  animatePress(userChosenColour);
   // console.log(userCLickedPattern);
+  checkAnswer(userCLickedPattern.length - 1);
 });
 
 // randomly generate integers from 0-3. This will help to select one of the four colors
 function nextSequence() {
-  $("#level-title").text("Level " + level);
-  level += 1;
+  userCLickedPattern = [];
+  level++;
+  $('#level-title').text('Level ' + level);
+
   var randomNumber = Math.floor(Math.round(Math.random() * 3));
 
   // choose a color
-  var randomChosenColour = buttonColours[nextSequence];
+  var randomChosenColour = buttonColours[randomNumber];
 
   // add to the simon sequence
   gamePattern.push(randomChosenColour);
@@ -44,12 +52,37 @@ function nextSequence() {
 
 };
 
-function checkAnswer(currentLevel) {
-  
+function startOver() {
+  console.log('wrong');
+  playSound('wrong');
+  $('body').addClass('game-over');
+  setTimeout(function () {
+    $('body').removeClass('game-over');
+  }, 200);
+  $("#level-title").text('Game Over, Press Any Key to Restart');
+  gamePattern = [];
+  userCLickedPattern = [];
+  level = 0;
+  started = false;
 }
 
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userCLickedPattern[currentLevel]) {
+    console.log('sucesss');
+    if (userCLickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    startOver();
+  }
+};
+
 $(document).keydown(function () {
-  if (started == false) {
+  if (!started) {
+    $("#level-title").text("Level " + level);
     nextSequence();
+    started = true;
   }
 })
